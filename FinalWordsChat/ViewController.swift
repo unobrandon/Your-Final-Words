4f7589596365fb87634c5f4f457565421164484b
Brandon Shaw <unobrandon@mac.com>
2018-06-27 13:05:11 -0400

    Initial Commit

---
+++ FinalWordsChat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FinalWordsChat
+//
+//  Created by Brandon Shaw on 6/27/18.
+//  Copyright © 2018 Brandon Shaw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
